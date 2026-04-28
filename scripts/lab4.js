577c81c18652be11873cc4ad95182cff4bb40477
dstyle294 <skdalal@ucsd.edu>
2026-04-28 16:39:50 -0700

    actual fixes

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,7 +7,7 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
-        const result = 0;
+        let result = 0;
 
         result = num1 + num2;
 
@@ -18,7 +18,6 @@ function sumValues(num1, num2, add) {
     }
 }
 
-// change to see if this does anything
 
 /**
  * 
@@ -31,7 +30,7 @@ function discountPrices(prices, discount) {
     const length = prices.length;
     let discountedPrice = 0
     for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+        discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
